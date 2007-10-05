dbed399356921e3450ea2c2781faed3661063dcb
(no author) <(no author)@548c6c19-a93b-0410-93bf-d61525b3de97>
2007-10-05 08:00:59 +0000

    removed database.yml, updated cap recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,11 +17,21 @@ role :db,  "eccles.spanner.org", :primary => true
 set :deploy_to, "/var/www/#{application}"
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 set :user, 'spanner'
+set :checkout, "export" 
+set :use_sudo, false
 
 namespace :deploy do
   task :after_update do
     sudo "ln -s #{shared_path}/assets/node #{current_release}/public/node" 
     sudo "ln -s #{shared_path}/assets/source #{current_release}/public/source" 
     sudo "ln -s #{shared_path}/assets/person #{current_release}/public/person"
+    run "ln -s #{shared_path}/config/database.yml #{current_release}/config/database.yml" 
+  end
+  task :spinner, :roles => :app do
+    send(run_method, “cd #{deploy_to}/#{current_dir} && mongrel_rails cluster::start”)
+  end
+  task :restart, :roles => :app do
+    send(run_method, “cd #{deploy_to}/#{current_dir} && mongrel_rails cluster::restart”)
   end
 end
+
