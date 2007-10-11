1b3c8d47a602e1ef4797a93a085244d4008f7a4a
(no author) <(no author)@548c6c19-a93b-0410-93bf-d61525b3de97>
2007-10-11 13:45:37 +0000

    blogging working, craply

--- app/models/blogentry.rb
+++ app/models/blogentry.rb
@@ -2,7 +2,6 @@ class Blogentry < ActiveRecord::Base
 
   belongs_to :creator, :class_name => 'User', :foreign_key => 'created_by'
   belongs_to :updater, :class_name => 'User', :foreign_key => 'updated_by'
-  belongs_to :speaker, :class_name => 'User', :foreign_key => 'speaker_id'
 
   belongs_to :node
   belongs_to :collection
@@ -11,7 +10,7 @@ class Blogentry < ActiveRecord::Base
   file_column :image, :magick => { 
     :versions => { 
       "thumb" => "56x56!", 
-      "slide" => "135x135!", 
+      "slide" => "175x175!", 
       "preview" => "750x540>" 
     }
   }
