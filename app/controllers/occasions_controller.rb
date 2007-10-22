c3df74f245013654d3f652baa36c05b23610f1d5
(no author) <(no author)@548c6c19-a93b-0410-93bf-d61525b3de97>
2007-10-22 16:12:38 +0000

    inline creation working. adding occasions

--- app/controllers/occasions_controller.rb
+++ app/controllers/occasions_controller.rb
@@ -1,65 +1,53 @@
-class UsersController < ApplicationController
+class OccasionsController < ApplicationController
   
   # this is user review and management for admins
   # logging in and registration is in account_controller
-
-  def limit_to_active_collection
-    ["collection_id = ? or status >= 200", current_collection]
-  end
 
   def index
     list
     render :action => 'list'
   end
-  
-  def list
-    @display = case params['display']
-      when "thumb" then "thumb"
-      when "slide" then "slide"
-      else "list"
-    end
-    perpage = params[:perpage] || (@display == 'thumb') ? 100 : 40
-    sort = case params['sort']
-           when "login" then "login"
-           when "status" then "status DESC"
-           when "email" then "email"
-           when "date" then "created_at DESC"
-           when "firstname" then "firstname, lastname"
-           else "lastname, firstname"
-           end
 
-    @users = User.find(:all, 
-      :conditions => limit_to_active_collection, 
-      :order => sort, 
-      :page => {
-        :size => perpage, 
-        :current => params[:page]
-      }
-    )
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+
+  def list
+   perpage = params[:perpage] || (@display == 'thumb') ? 100 : 40
+   sort = case params[:sort]
+     when "name"  then "name"
+     when "date" then "date DESC"
+     when "name_reverse" then "name DESC"
+     when "date_reverse" then "date"
+     else "name"
+   end
+   @sources = Source.find(:all, :conditions => limit_to_active_collection, :page => {:size => perpage, :sort => sort, :current => params[:page]})
+   @display = case params['display']
+     when "thumb" then "thumb"
+     when "slide" then "slide"
+     else "list"
+   end
   end
   
   def show
-    userid = params[:id] || $current_user.id
-    @user = User.find(userid)
-    @users = User.find(:all)
+    @occasion = Occasion.find(params[:id])
   end
 
   def new
-    @user = User.new
+    @occasion = Occasion.new
     respond_to do |format|
       format.html
-      format.js { render :layout => false, :template => 'users/new_inline' }
+      format.js { render :layout => false, :template => 'occasions/new_inline' }
     end
   end
 
   def create
-    model = params[:user][:login] ? LoginUser : User
-    @user = model.new(params[:user])
-    if @user.save
-      flash[:notice] = 'User created.'
+    @occasion = Occasion.new(params[:occasion])
+    if @occasion.save
+      flash[:notice] = 'Occasion created.'
       respond_to do |format|
         format.html
-        format.js { render :layout => false, :template => 'users/created_inline' }
+        format.js { render :layout => false, :template => 'occasions/created_inline' }
       end
     else
       render :action => 'new'
@@ -67,19 +55,22 @@ class UsersController < ApplicationController
   end
 
   def edit
-    userid = params[:id] || $current_user.id
-    @user = User.find(userid)
+    @occasion = Occasion.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:user])
-    if @user.update_attributes(params[:user])
-      flash[:notice] = 'User was updated.'
-      redirect_to :action => 'show', :id => @user
+    @occasion = Occasion.find(params[:id])
+    if @occasion.update_attributes(params[:occasion])
+      flash[:notice] = 'Occasion successfully updated.'
+      redirect_to :action => 'show', :id => @occasion
     else
-      flash[:notice] = 'failed to update.'
       render :action => 'edit'
     end
   end
-
+  
+  def destroy
+    Occasion.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
+  
 end
