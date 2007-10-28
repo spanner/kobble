fea079c9775d1cf1b6c8f06b525611d44686e0a5
(no author) <(no author)@548c6c19-a93b-0410-93bf-d61525b3de97>
2007-10-28 16:09:20 +0000

    time_ago_in_words stupid beast subroutine removed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,24 +46,5 @@ module ApplicationHelper
     options[:q] ? all_search_posts_path(options) : send("#{prefix}all_posts_path", options)
   end
 
-  # on windows and this isn't working like you expect?
-  # check: http://beast.caboo.se/forums/1/topics/657
-  # strftime on windows doesn't seem to support %e and you'll need to 
-  # use the less cool %d in the strftime line below
-  def distance_of_time_in_words(from_time, to_time = 0, include_seconds = false)
-    from_time = from_time.to_time if from_time.respond_to?(:to_time)
-    to_time = to_time.to_time if to_time.respond_to?(:to_time)
-    distance_in_minutes = (((to_time - from_time).abs)/60).round
-  
-    case distance_in_minutes
-      when 0..1           then (distance_in_minutes==0) ? 'a few seconds ago' : '1 minute ago'
-      when 2..59          then "#{distance_in_minutes} minutes ago"
-      when 60..90         then "1 hour ago"
-      when 90..1440       then "#{(distance_in_minutes.to_f / 60.0).round} hours ago"
-      when 1440..2160     then '1 day ago' # 1 day to 1.5 days
-      when 2160..2880     then "#{(distance_in_minutes.to_f / 1440.0).round} days ago" # 1.5 days to 2 days
-      else from_time.strftime("%b %e, %Y  %l:%M%p"[:datetime_format]).gsub(/([AP]M)/) { |x| x.downcase }
-    end
-  end
   
 end
