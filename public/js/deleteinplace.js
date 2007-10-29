6e3e4d43c57e9ac9f7575abbb1e4f88eff3f8b05
(no author) <(no author)@548c6c19-a93b-0410-93bf-d61525b3de97>
2007-10-29 10:24:06 +0000

    ie7 working without errors

---
+++ public/js/deleteinplace.js
@@ -0,0 +1,54 @@
+﻿var Deleter= new Class({
+  initialize: function (a, e) {
+    this.link = a;
+    this.signal = $E('img', a);
+    this.subject = $E('#' + a.id.replace('delete_',''));
+    this.fader = new Fx.Styles(this.subject, {duration:400});
+    this.req = null;
+    if (this.confirm()) this.delete();
+  },
+
+  request_url: function () {
+    return this.link.getProperty('href');
+  },
+  
+  title: function (argument) {
+    return this.link.getProperty('title');
+  },
+
+  confirm: function () {
+    return confirm("are you sure you want to " + this.title() + '?');
+  },
+  
+  delete: function () {
+    del = this;
+		this.req = new Ajax(this.request_url(), {
+			method: 'get',
+		  onRequest: function () {del.waiting();},
+		  onComplete: function () {del.finished();},
+		  onFailure: function () {del.failed();}
+		}).request();
+  }, 
+  
+  waiting: function () {
+    this.signal.setProperty('src', '/images/furniture/signals/wait_16_blue.gif');
+  },
+  
+  notWaiting: function () {
+    this.signal.setProperty('src', '/images/furniture/buttons/delete.png');
+  },
+  
+  finished: function (argument) {
+    console.log('finished!');
+    this.fader.start({
+      'opacity': 0,
+      'height': 0
+    }).chain(function () { this.element.remove() })
+  },
+  
+  failed: function (argument) {
+    this.notWaiting();
+    alert('deletion failed');
+  }
+  
+});
--- public/js/mlal.js
+++ public/js/mlal.js
@@ -1,15 +1,4 @@
-﻿function flash (element) {
-  var flashfx = new Fx.Styles(element, {duration:1000, wait:false});
-  var bgbackto = element.getStyle('background-color');
-  if (bgbackto == 'transparent') bgbackto = '#ffffff';
-  var fgbackto = element.getStyle('color');
-  flashfx.start({
-		'background-color': ['#CC6E1F',bgbackto],
-		'color': ['#CC6E1F',fgbackto]
-  });
-}
-
-window.addEvent('domready', function(){
+﻿window.addEvent('domready', function(){
   $ES('a.editinplace').each( function (element) {
     element.addEvent('click', function (e) {
       e = new Event(e).stop();
