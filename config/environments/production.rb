a88d2af4160502345f839f3189ce89320bf41e76
(no author) <(no author)@548c6c19-a93b-0410-93bf-d61525b3de97>
2007-11-24 01:00:49 +0000

    email failures unreported

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ config.action_controller.perform_caching             = true
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors if you bad email addresses should just be ignored
-config.action_mailer.raise_delivery_errors = true
 
+config.action_mailer.raise_delivery_errors = false
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.server_settings = {
     :address        => 'seagoon.spanner.org',
