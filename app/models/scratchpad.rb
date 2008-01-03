715223c04a9d9d5d867bab5f2caa779511b13f35
(no author) <(no author)@548c6c19-a93b-0410-93bf-d61525b3de97>
2008-01-03 17:06:20 +0000

    rename twiddling

--- app/models/scratchpad.rb
+++ app/models/scratchpad.rb
@@ -1,5 +1,5 @@
 class Scratchpad < ActiveRecord::Base
-  belongs_to :collection, :class_name => 'User', :foreign_key => 'created_by'
+  belongs_to :collection
   belongs_to :user, :class_name => 'User', :foreign_key => 'created_by'
   belongs_to :updater, :class_name => 'User', :foreign_key => 'updated_by'
 
