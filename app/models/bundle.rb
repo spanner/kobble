5644252285055169d63d377bd5a867728cbda7a7
William Ross <will@spanner.org>
2008-03-27 09:10:14 +0000

    still bug hunting

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -1,7 +1,7 @@
 class Bundle < ActiveRecord::Base
 
   has_many_polymorphs :members, :as => 'superbundle', :from => self.organised_classes, :through => :bundlings, :dependent => :destroy
-  acts_as_catcher :members
+#  acts_as_catcher :members
   acts_as_spoke
   
   def self.index_fields
