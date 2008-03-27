f52a716ffaef56052e157346fff4e98b61698ee8
William Ross <will@spanner.org>
2008-03-27 09:59:11 +0000

    still bug hunting

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -4,19 +4,19 @@ class Bundle < ActiveRecord::Base
   # has_many_polymorphs :members, :as => 'superbundle', :from => self.organised_classes, :through => :bundlings, :dependent => :destroy
   # acts_as_spoke
   # acts_as_catcher :members
-  # 
-  # def self.index_fields
-  #   STDERR.puts "%%% Bundle.index_fields (self is #{self})"
-  #   ['name', 'description', 'body', 'created_by', 'created_at', 'collection_id']
-  # end
-  # 
-  # def self.index_concatenation
-  #   STDERR.puts "%%% Bundle.index_concatenation (self is #{self})"
-  #   [{:fields => ['observations', 'arising', 'emotions'], :as => 'field_notes'}]
-  # end
-  # 
-  # def self.nice_title
-  #   "set"
-  # end
+  
+  def self.index_fields
+    STDERR.puts "%%% Bundle.index_fields (self is #{self})"
+    ['name', 'description', 'body', 'created_by', 'created_at', 'collection_id']
+  end
+
+  def self.index_concatenation
+    STDERR.puts "%%% Bundle.index_concatenation (self is #{self})"
+    [{:fields => ['observations', 'arising', 'emotions'], :as => 'field_notes'}]
+  end
+  
+  def self.nice_title
+    "set"
+  end
 end
 
