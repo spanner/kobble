54605d777d1c476ace3a36fc4dddb26e4b9003dd
William Ross <will@spanner.org>
2008-03-27 10:01:10 +0000

    still bug hunting

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -1,6 +1,6 @@
 class Bundle < ActiveRecord::Base
 
-  # has_many :bundlings, :dependent => :destroy, :as => 'superbundle'
+  has_many :bundlings, :dependent => :destroy, :as => 'superbundle'
   # has_many_polymorphs :members, :as => 'superbundle', :from => self.organised_classes, :through => :bundlings, :dependent => :destroy
   acts_as_spoke
   acts_as_catcher :members
