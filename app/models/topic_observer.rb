712fa3e04fdd2167e160ff63b216426f13ea2ada
William Ross <will@spanner.org>
2008-08-20 13:34:41 +0000

    small fix in topic observer

--- app/models/topic_observer.rb
+++ app/models/topic_observer.rb
@@ -3,7 +3,7 @@ class TopicObserver < ActiveRecord::Observer
   def after_create(topic)
     topic.collection = topic.referent.collection if topic.referent.has_collection?
     monitors = topic.monitors + User.everything_monitors
-    monitors.push topic.referent.creator if topic.referent.creator.prefers? 'notify_comments'
+    # monitors.push topic.referent.creator if topic.referent.creator.prefers? 'notify_comments'
     monitors.each do |user|
       UserNotifier.deliver_topic_notification(user, topic) unless user == topic.creator
     end
