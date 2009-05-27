72e58c7b143da979390a18563d775e22264ac055
will <will@spanner.org>
2009-05-27 12:56:29 +0100

    cascading undeleted

--- vendor/plugins/material/lib/material.rb
+++ vendor/plugins/material/lib/material.rb
@@ -3,12 +3,14 @@ module Material
   gem 'mislav-will_paginate'
   require 'material/polymorphs'
   require 'material/string_extensions'
+  require 'material/aap_extensions'
   require 'material/catch_response'
   require 'material/catch_error'
   require 'material/catcher'
   require 'material/core'
   
   String.send :include, Material::StringExtensions
+  ActiveRecord::Base.send :include, Material::AapExtensions
   ActiveRecord::Base.send :include, Material::Catcher
   ActiveRecord::Base.send :include, Material::Core
   
