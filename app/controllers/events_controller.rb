c89043a29a6dcbab9621ad1efaa0b2c3f1252e2e
will <will@spanner.org>
2009-06-05 20:31:43 +0100

    list structure and presentation cleaner: ready for drag-revamp

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,40 +1,12 @@
-class EventsController < ApplicationController
-  before_filter :find_account_or_collection_or_user
+class EventsController < CollectionScopedController
 
   def index
-    @thing = @collection || @user || @account
+    @user = current_account.users.find(params[:user_id]) if params[:user_id]
+    @thing = @user || current_collection
     if params[:type] && ['created', 'updated', 'deleted'].include?(params[:type])
       @events = @thing.events.of_type(params[:type]).paginate(self.paging)
     else
       @events = @thing.events.paginate(self.paging)
-    end
-  end
-
-  def show
-    @thing = @account.events.find(params[:id])
-  end
-  
-  def views
-    ['index']
-  end
-
-  private
-  
-  def find_account_or_collection_or_user
-    if params[:account_id]
-      access_insufficient unless admin?
-      @account = Account.find(params[:account_id])
-      @scope = 'account'
-    else
-      @account = current_account
-    end
-    if params[:collection_id]
-      @collection = @account.collections.find(params[:collection_id])
-      @scope = 'collection'
-    end
-    if params[:user_id]
-      @user = @account.users.find(params[:user_id])
-      @scope = 'user'
     end
   end
   
