9b846cb49d0fec2d9aff685aedb36bd1ecf66942
will <will@spanner.org>
2009-06-07 11:11:06 +0100

    stylesheet broken up into sass imports

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,13 +13,13 @@ role :db,  "eccles.spanner.org", :primary => true
 
 set :deploy_to, "/var/www/#{application}"
 set :deploy_via, :remote_cache
-set :classes_with_assets, %w{account bundle collection node occasion person source tag user}
+set :asset_directories, %w{accounts bundles collections nodes occasions people sources tags users}
 
 default_run_options[:pty] = true
 
 after "deploy:setup" do
   sudo "mkdir -p #{shared_path}/assets #{shared_path}/config #{deploy_to}/logs"
-  classes_with_assets.each { |directory| sudo "mkdir #{shared_path}/assets/#{directory}" }
+  asset_directories.each { |directory| sudo "mkdir #{shared_path}/assets/#{directory}" }
   sudo "chown -R #{user}:#{group} #{shared_path}"
   sudo "chown #{user}:#{group} #{deploy_to}/releases"
   sudo "ln -s #{shared_path}/config/nginx.conf /etc/nginx/sites-available/#{application}"
@@ -27,7 +27,7 @@ end
 
 after "deploy:update" do
   run "ln -s #{shared_path}/config/database.yml #{current_release}/config/database.yml" 
-  classes_with_assets.each do |directory|
+  asset_directories.each do |directory|
     run "ln -s #{shared_path}/assets/#{directory} #{current_release}/public/#{directory}" 
   end
 end
