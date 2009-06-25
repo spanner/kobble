fbc8142b49962a127feaf2d2ab14a5889590917f
will <will@spanner.org>
2009-06-25 12:12:06 +0100

    popular tag list in bench

---
+++ public/javascripts/kobble/tabs.js
@@ -0,0 +1,67 @@
+var tabs = [];
+var tabsets = {};
+
+var Tab = new Class({
+	initialize: function(element){
+		this.tabhead = element;
+		this.name = this.tabhead.get('text');
+    var parts = element.id.split('_');
+		this.tag = parts.pop();
+		var settag = parts.pop();
+		this.tabbody = $(settag + '_' + this.tag);
+    this.tabset = tabsets[settag] || new TabSet(settag);
+    this.tabset.addTab(this);
+ 		this.tabhead.onclick = this.select.bind(this);
+ 		tabs.push(this);
+	},
+	select: function (e) {
+    k.block(e);
+	  this.tabhead.blur();
+    this.tabset.select(this);
+	},
+  show: function(){
+    this.tabhead.addClass('fg');
+    this.tabbody.show();
+  },
+  hide: function(){
+    this.tabbody.hide();
+    this.tabhead.removeClass('fg');
+  }
+});
+
+var TabSet = new Class({
+	initialize: function(tag){
+	  this.tabs = [];
+    this.tag = tag;
+    this.foreground = null;
+    this.rolling = false;
+ 		tabsets[this.tag] = this;
+	},
+	addTab: function (tab) {
+    this.tabs.push(tab);
+    if (this.tabs.length == 1) {
+      tab.show();
+      this.foreground = tab;
+    } else {
+      tab.hide();
+    }
+	},
+	select: function (tab) {
+	  this.tabs.each(function (t) { 
+	    if (t.tag == tab.tag) {
+        console.log('selecting ', t.name);
+	      t.show();
+	      this.foreground = t;
+	    } else {
+        console.log('not ', t.name);
+        t.hide();
+	    }
+	  }, this);
+	}
+});
+
+kobble_starters.push(function (scope) {
+  if (!scope) scope = document;
+  scope.getElementsIncludingSelf('a.tab').each (function (el) { new Tab(el); });
+});
+
